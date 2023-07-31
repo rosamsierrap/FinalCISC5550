ec3a1d0f60e043597357b2be0a7497cb624993fb
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-07-31 18:58:36 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,10 +2,3 @@
 :: Set the project to work on
 gcloud config set project my-project-cisc5550
 
-:: Create the virtual machine instance
-gcloud compute instances create instance-2 ^
-    --machine-type=e2-medium ^
-    --image-family=ubuntu-2004-lts ^
-    --image-project=ubuntu-os-cloud ^
-    --tags=http-server ^
-    --zone=us-central1-a
