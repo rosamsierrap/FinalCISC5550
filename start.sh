a0262d0c367a05f194cf94c1574c574458b4daf8
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-07-31 16:35:55 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,7 +17,9 @@ gcloud compute instances create instance-2 \
     
 #connect to the VM
 gcloud compute ssh instance-2 --zone=us-central1-a \
-   --command="sudo apt update && sudo apt install -y python3-pip && sudo pip3 install flask && sudo -H pip3 install flask && git clone https://github.com/rosamsierrap/cisc5550.git && sed -i 's|app.run("0.0.0.0")|app.run("0.0.0.0", port=80)|' todolist.py && sudo python3 ~/todolist.py &"
+   --command="sudo apt update && sudo apt install -y python3-pip && sudo pip3 install flask && sudo -H pip3 install flask
+   && git clone https://github.com/rosamsierrap/cisc5550.git && sed -i 's|app.run("0.0.0.0")|app.run("0.0.0.0", port=80)|' todolist.py 
+   && sudo python3 ~/todolist.py &"
 
 
 #installing libraries on the VM
