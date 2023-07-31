6730e062f7e43f605863f25bcda505aaa93320bb
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-07-31 15:52:16 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#select project to work on
+gcloud config set project my-project-cisc5550
+
+#create the virtual machine instance
+gcloud compute instances create instance-2 \
+    --machine-type=e2-medium \
+    --image-family=debian-10 \
+    --image-project=debian-cloud \
+    --tags=http-server \
+    --zone=us-central1-a
+    
+#connect to the VM
+gcloud compute ssh instance-2 --zone=us-central1-a
+
+#Test the app on Google Cloud
+sudo python3 todolist.py &
