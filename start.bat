74dd5e751940a2e98351eea71ada60e469b76ec2
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-07-31 18:53:02 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -9,7 +9,3 @@ gcloud compute instances create instance-2 ^
     --image-project=ubuntu-os-cloud ^
     --tags=http-server ^
     --zone=us-central1-a
-
-:: Connect to the VM and execute commands
-gcloud compute ssh instance-2 --zone=us-central1-a ^
-   --command="sudo apt update && sudo apt install -y python3-pip && sudo pip3 install flask && sudo -H pip3 install flask && git clone https://github.com/rosamsierrap/cisc5550.git && sed -i 's|app.run(\"0.0.0.0\")|app.run(\"0.0.0.0\", port=80)|' cisc5550/todolist.py && sudo python3 ~/cisc5550/todolist.py &"
