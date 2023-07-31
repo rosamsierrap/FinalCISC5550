492b1676556858d377536bf6976b3027cc0dfc11
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-07-31 19:03:54 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 
 :: Set the project to work on
-gcloud config set project my-project-cisc5550
+gcloud config set project my-project-cisc5550 --quiet
+
+gcloud config set compute/zone us-central1-a --quiet
 
