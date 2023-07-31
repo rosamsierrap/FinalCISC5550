ef80cdce6249c507550fe589287f4732ee86c1ca
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-07-31 16:07:27 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,18 @@ gcloud compute instances create instance-2 \
     --zone=us-central1-a
     
 #connect to the VM
-gcloud compute ssh instance-2 --zone=us-central1-a
+gcloud compute ssh instance-2 --zone=us-central1-a << EOF
+
+#installing libraries on the VM
+sudo apt update
+sudo apt install python3-pip
+pip3 install flask
+sudo -H pip3 install flask
+
+#Update the last line of the todolist.py file
+sed -i 's|app.run("0.0.0.0")|app.run("0.0.0.0", port=80)|' ~/todolist.py
 
 #Test the app on Google Cloud
-sudo python todolist.py &
+sudo python3 todolist.py &
+
+EOF
