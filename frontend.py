30812a6065d9cf59410270815f040be37c5441d6
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-08-02 21:51:20 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -9,6 +9,29 @@ def show_list():
       resp = resp.json()
       return render_template('index.html', todolist=resp)
 
+@app.route("/add", methods=['POST'])
+def add_entry():
+    data = {
+        "what_to_do": request.form['what_to_do'],
+        "due_date": request.form['due_date']
+    }
+    response = requests.post("http://localhost:5001/api/add", json=data)
+    return redirect("/")
 
+@app.route("/delete/<item>")
+def delete_entry(item):
+    response = requests.delete(f"http://localhost:5001/api/delete/{item}")
+    return redirect("/")
+
+@app.route("/mark/<item>")
+def mark_as_done(item):
+    response = requests.put(f"http://localhost:5001/api/mark/{item}")
+    return redirect("/")
+
+@app.teardown_appcontext
+def close_request(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+          
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run("0.0.0.0", port=80)
