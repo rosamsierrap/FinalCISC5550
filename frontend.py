23955d44942ea28f8f930b2f2684420c66518cdd
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-08-03 17:49:14 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for,g
+from flask import Flask, render_template, redirect, request, url_for, g
 import requests
 
 app = Flask(__name__)
