ebcde64e5be71da5a2d71df726a379bcfeefde7f
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-08-08 00:18:52 -0400

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 TODO_API_URL = "http://"+os.environ['TODO_API_IP']+":5001"
 
 
+@app.route("/login", methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+
 @app.route("/")
 def show_list():
     resp = requests.get(TODO_API_URL+"/api/items")
@@ -15,25 +20,38 @@ def show_list():
     return render_template('index.html', todolist=resp)
 
 
+# Add authentication checks for protected routes
 @app.route("/add", methods=['POST'])
 def add_entry():
-    requests.post(TODO_API_URL+"/api/items", json={
+    if 'username' in session:
+        requests.post(TODO_API_URL+"/api/items", json={
                   "what_to_do": request.form['what_to_do'], "due_date": request.form['due_date']})
-    return redirect(url_for('show_list'))
+        return redirect(url_for('show_list'))
+    else:
+        flash('Please log in to add tasks', 'error')
+        return redirect(url_for('login_page'))
 
 
 @app.route("/delete/<item>")
 def delete_entry(item):
-    item = urllib.parse.quote(item)
-    requests.delete(TODO_API_URL+"/api/items/"+item)
-    return redirect(url_for('show_list'))
+    if 'username' in session:
+        item = urllib.parse.quote(item)
+        requests.delete(TODO_API_URL+"/api/items/"+item)
+        return redirect(url_for('show_list'))
+    else:
+        flash('Please log in to delete tasks', 'error')
+        return redirect(url_for('login_page'))
 
 
 @app.route("/mark/<item>")
 def mark_as_done(item):
-    item = urllib.parse.quote(item)
-    requests.put(TODO_API_URL+"/api/items/"+item)
-    return redirect(url_for('show_list'))
+    if 'username' in session:
+        item = urllib.parse.quote(item)
+        requests.put(TODO_API_URL+"/api/items/"+item)
+        return redirect(url_for('show_list'))
+    else:
+        flash('Please log in to mark tasks', 'error')
+        return redirect(url_for('login_page'))
 
 
 if __name__ == "__main__":
